46143571d47edea60507a6e446151a3845e7bc7a
Linkdude09 <belac.ybloc@gmail.com>
2018-03-02 17:49:51 -0700

    decent fxml layout added

--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -1,21 +1,26 @@
 package controllers;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage stage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			GridPane root = FXMLLoader.load(getClass().getResource("FXML-FFormula.fxml"));
+			
+			Scene scene = new Scene(root,1000,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+			
+			stage.setScene(scene);
+			stage.setResizable(false);
+			stage.setTitle("Foodie's Formula");
+			stage.show();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
