fc542e9916db53ec3aa94caef57a07e3835aae87
Linkdude09 <belac.ybloc@gmail.com>
2018-03-05 16:52:31 -0700

    minor gui fix

--- src/controllers/GUIController.java
+++ src/controllers/GUIController.java
@@ -8,54 +8,101 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class GUIController implements Initializable{
+public class GUIController implements Initializable {
 
-    @FXML
-    private TableView<String> recipeTable;
+	@FXML
+	private TableView<String> recipeTable;
 
-    @FXML
-    private ComboBox<String> unitComboBox;
+	@FXML
+	private ComboBox<String> unitComboBox;
 
-    @FXML
-    private TextField ingredientTextField;
+	@FXML
+	private TextField ingredientTextField;
 
-    @FXML
-    private Spinner<Integer> amountSpinner;
+	@FXML
+	private Spinner<Integer> amountSpinner = new Spinner<Integer>(0, 100, 0);
 
-    @FXML
-    private Button ingredientButton;
+	@FXML
+	private Button ingredientButton;
 
-    @FXML
-    private TableView<String> shoppingListTable;
+	@FXML
+	private TableView<String> shoppingListTable;
 
-    @FXML
-    private TextField recipeNameField;
+	@FXML
+	private TextField recipeNameField;
 
-    @FXML
-    private TextArea recipeInsructionsField;
+	@FXML
+	private TextArea recipeInsructionsField;
 
-    @FXML
-    private Button saveRecipeButton;
+	@FXML
+	private Button saveRecipeButton;
 
-    @FXML
-    private Button convertButton;
+	@FXML
+	private Button convertButton;
 
-    @FXML
-    private TextField searchField;
+	@FXML
+	private TextField searchField;
 
-    @FXML
-    private Button searchButton;
-    
+	@FXML
+	private Button searchButton;
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		unitComboBox.getItems().addAll("Whole","Quart","Cup(s)", "Tbsp", "tsp","mL","fl. oz.");
-		//add more units here to the ComboBox as we need them
+		unitComboBox.getItems().addAll("Whole", "Quart", "Cup(s)", "Tbsp", "tsp", "mL", "fl. oz.");
+		// add more units here to the ComboBox as we need them
+		searchButton.onActionProperty();
+		
 	}
-
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
